e46bbfeb61b2d882ed261a6eaf5c8f7a0c7cccf4
SAMAR2101 <samarmiled864@gmail.com>
2024-12-07 18:26:42 +0100

    premier commit

---
+++ evenadd.js
@@ -0,0 +1,6 @@
+const evenOrOdd = (num) => {
+    return num % 2 === 0 ? "Even" : "Odd";
+  };
+  
+  console.log(evenOrOdd(8)); 
+  console.log(evenOrOdd(5)); 
---
+++ repeat.js
@@ -0,0 +1,5 @@
+
+
+let str = prompt("Please enter your word");
+let repeatCount = str.repeat(5);
+alert(repeatCount);
